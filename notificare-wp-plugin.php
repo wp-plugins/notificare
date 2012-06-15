b71891132ea7b996e6f253e703a70f929e88bd61
silentjohnny <silentjohnny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-15 11:03:37 +0000

    Added some info to readme
    git-svn-id: https://plugins.svn.wordpress.org/notificare/trunk@558456 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- notificare-wp-plugin.php
+++ notificare-wp-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Notificare
 Plugin URI: http://notifica.re/apps/wordpress
 Description: Get notified on comments and approve or mark as spam with a simple push of a button from your phone
-Version: 0.1.1
+Version: 0.1.2
 Author: silentjohnny
 License: 
 
